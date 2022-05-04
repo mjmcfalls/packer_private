6bdfad1f471754b3fd31c11b26297dfec4a7c314
Matthew M <514050+mjmcfalls@users.noreply.github.com>
2022-05-04 15:37:57 -0400

    Update chrome logic and startmenulayout

--- src/scripts/install_Chrome.ps1
+++ src/scripts/install_Chrome.ps1
@@ -52,26 +52,26 @@ Function Write-Log {
 $ProgressPreference = 'SilentlyContinue'
 
 Create-TempFolder -Path $outpath
+$installerPath = Join-Path -Path $outpath -ChildPath $installername
 
 if ($public.IsPresent) {
     Write-Log -Level "INFO" -Message "Chrome - Install from Web"
-    Invoke-WebRequest -Uri "$($uri)" -OutFile (Join-Path -Path $outpath -ChildPath $installername)  -UseBasicParsing
+    Invoke-WebRequest -Uri "$($uri)" -OutFile $installerPath  -UseBasicParsing
 }
 else {
     Write-Log -Level "INFO" -Message "Getting $($uri)$($appuri)$($installername)"
-    Invoke-WebRequest -Uri "$($uri)$($appuri)$($installername)" -OutFile (Join-Path -Path $outpath -ChildPath $installername)  -UseBasicParsing
+    Invoke-WebRequest -Uri "$($uri)$($appuri)$($installername)" -OutFile $installerPath  -UseBasicParsing
 }
 
 if ($install.IsPresent) {
-    $ = $installername -Split ".zip"
+    $archiveDestination = $installername -Split ".zip"
 
     Write-Log -Level "INFO" -Message "Unzipping $($installername) to $(Join-Path -Path $outpath -ChildPath $archiveDestination)"
-    Expand-Archive -Path "$($installername)" -Destination (Join-Path -Path $outpath -ChildPath $archiveDestination) -Force
+    Expand-Archive -Path "$($installerPath)" -Destination (Join-Path -Path $outpath -ChildPath $archiveDestination) -Force
 
     Write-Log -Level "INFO" -Message "Finding Chrome Installer"
-    $installer = Get-Childitem (Join-Path -Path $outpath -ChildPath $installername) -Filter $fileFilter
+    $installer = Get-Childitem $installerPath -Filter $fileFilter
     
-
     Write-Log -Level "INFO" -Message "Starting Install"
     Write-Log -Level "INFO" -Message "Start-Process -NoNewWindow -FilePath $($env:systemroot)\system32\msiexec.exe -ArgumentList `"/package $($installer.FullName) $($installParams)`""
     Start-Process -NoNewWindow -FilePath "$($env:systemroot)\system32\msiexec.exe" -ArgumentList "/package $($installer.FullName) $($installParams)"
