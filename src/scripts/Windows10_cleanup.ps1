4e8a076958137d91a3f6135909b9cbcd2f4c0987
Matthew M <514050+mjmcfalls@users.noreply.github.com>
2022-05-06 12:59:01 -0400

    Add logic to remove Xbox apps

--- src/scripts/Windows10_cleanup.ps1
+++ src/scripts/Windows10_cleanup.ps1
@@ -2,6 +2,7 @@
 # Remove AppX Packages
 Get-AppxPackage | Where-Object {$_.Name -notlike "*Search*" -or $_.Name -notlike "*Calc*" -or $_.Name -notlike "*Store*"} | Remove-AppXPackage
 
+dism /Online /Get-ProvisionedAppxPackages | Select-String PackageName | Select-String xbox | ForEach-Object {$_.Line.Split(':')[1].Trim()} | ForEach-Object { dism /Online /Remove-ProvisionedAppxPackage /PackageName:$_ }
 
 # Remove OneDrive Setup 
 takeown /F $mountdir\Windows\SysWOW64\OneDriveSetup.exe /A
