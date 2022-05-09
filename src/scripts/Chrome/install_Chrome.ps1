46122dc82fc22a5e22d0954f6231d4f295aa901e
Matthew M <514050+mjmcfalls@users.noreply.github.com>
2022-05-09 15:43:19 -0400

    Add qemu file; comment out bad copy line

--- src/scripts/Chrome/install_Chrome.ps1
+++ src/scripts/Chrome/install_Chrome.ps1
@@ -74,7 +74,7 @@ if ($install.IsPresent) {
     if ($preferenceFileState) {
         Write-Log -Level "INFO" -Message "Found - $($preferenceFileState.FullName)"
         Write-Log -Level "INFO" -Message "Copy-Item -Path $($preferenceFileState.FullName) -Destination $($preferenceFileDest) -Force"
-        Copy-Item -Path $preferenceFileState.FullName -Destination $preferenceFileDest -Force
+        # Copy-Item -Path $preferenceFileState.FullName -Destination $preferenceFileDest -Force
 
         Write-Log -Level "INFO" -Message "Copy-Item -Path $($preferenceFileState.FullName) -Destination $(Join-Path -Path $installerPath -ChildPath "master_preferences") -Force"
         Copy-Item -Path $preferenceFileState.FullName -Destination (Join-Path -Path $installerPath -ChildPath "master_preferences") -Force
