9eeceeaaefe3b657a00c794891ed6f33b06b5612
Matthew M <514050+mjmcfalls@users.noreply.github.com>
2022-05-11 10:30:04 -0400

    add broken build script

---
+++ build_packer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+current_date=$(date "+%Y%m%d%H%M%S")
+file_name=packer_build.$current_date.log
+
+export PACKER_LOG="1"
+export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/$file_name"
+
+
