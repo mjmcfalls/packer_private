67eef27f4fc8bc76ae365bc9672bc766992963f4
Matthew M <514050+mjmcfalls@users.noreply.github.com>
2022-05-12 11:08:15 -0400

    add dot net recomplie; comment out silentlycontinue

--- src/scripts/Win10_Packer_cleanup.ps1
+++ src/scripts/Win10_Packer_cleanup.ps1
@@ -33,6 +33,14 @@ $tempPaths = New-Object System.Collections.Generic.List[System.Object]
 $tempPaths.Add($env:temp)
 $tempPaths.Add($tempDir)
 
+# Recomplie Dot Net x64
+Write-Log -Level "INFO" -Message "Recompiling x64 dot net"
+&"c:\windows\microsoft.net\framework64\v4.0.30319\ngen.exe update /force"
+
+# Recomplie Dot Net x86
+Write-Log -Level "INFO" -Message "Recompiling x86 dot net"
+&"c:\windows\microsoft.net\framework\v4.0.30319\ngen.exe update /force"
+
 foreach($tpath in $tempPaths){
     Write-Log -Level "INFO" -Message "Cleaning $($tpath)"
 
@@ -40,11 +48,11 @@ foreach($tpath in $tempPaths){
     $tempFiles = Get-ChildItem $tpath -Recurse -File
     # $tempFiles
     Write-Log -Level "INFO" -Message "Removing files in $($tpath)"
-    $tempFiles | Remove-Item -Force -ErrorAction SilentlyContinue
+    $tempFiles | Remove-Item -Force #-ErrorAction SilentlyContinue
 
     Write-Log -Level "INFO" -Message "Getting Directories in $($tpath)"
     $tempDirs = Get-ChildItem $tpath -Recurse -Directory
     # $tempFiles
     Write-Log -Level "INFO" -Message "Removing Directories in $($tpath)"
-    $tempDirs | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+    $tempDirs | Remove-Item -Force -Recurse #-ErrorAction SilentlyContinue
 }
