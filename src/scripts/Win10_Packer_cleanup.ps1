1778da37f5e1f3e8c51d151b87f076f96c307b96
Matthew M <514050+mjmcfalls@users.noreply.github.com>
2022-05-12 12:02:53 -0400

    Update cleanup script to zero free space

--- src/scripts/Win10_Packer_cleanup.ps1
+++ src/scripts/Win10_Packer_cleanup.ps1
@@ -1,6 +1,7 @@
 [CmdletBinding()]
 Param (
-    [string]$tempDir = "c:\temp"
+    [string]$outPath = "c:\temp",
+    [string]$sdelete_uri = "https://download.sysinternals.com/files/SDelete.zip"
 )
 
 Function Write-Log {
@@ -29,18 +30,29 @@ Function Write-Log {
 }
 
 
-$tempPaths = New-Object System.Collections.Generic.List[System.Object]
-$tempPaths.Add($env:temp)
-$tempPaths.Add($tempDir)
-
 # Recomplie Dot Net x64
 Write-Log -Level "INFO" -Message "Recompiling x64 dot net"
 Start-Process -NoNewWindow -FilePath "c:\windows\microsoft.net\framework64\v4.0.30319\ngen.exe"  -ArgumentList "update /force" -Wait
-&
+
 
 # Recomplie Dot Net x86
 Write-Log -Level "INFO" -Message "Recompiling x86 dot net"
 Start-Process -NoNewWindow -FilePath "c:\windows\microsoft.net\framework\v4.0.30319\ngen.exe "  -ArgumentList "update /force" -Wait
+
+
+
+# Clean-up Online image
+Write-Log -Level "INFO" -Message "Running Dism.exe /online /Cleanup-Image /StartComponentCleanup"
+Start-Process -NoNewWindow -FilePath "Dism.exe" -ArgumentList "/online /Cleanup-Image /StartComponentCleanup"
+
+# Clean-up and remove all superseded versions of every component in the component store
+# Write-Log -Level "INFO" -Message "Running Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase"
+# Start-Process -NoNewWindow -FilePath "Dism.exe" -ArgumentList "/online /Cleanup-Image /StartComponentCleanup /ResetBase"
+
+
+$tempPaths = New-Object System.Collections.Generic.List[System.Object]
+$tempPaths.Add($env:temp)
+$tempPaths.Add($outPath)
 
 foreach($tpath in $tempPaths){
     Write-Log -Level "INFO" -Message "Cleaning $($tpath)"
@@ -58,10 +70,26 @@ foreach($tpath in $tempPaths){
     $tempDirs | Remove-Item -Force -Recurse #-ErrorAction SilentlyContinue
 }
 
+Write-Log -Level "INFO" -Message "Downloading Sdelete"
+Invoke-WebRequest -Uri $sdelete_uri -OutFile (Join-Path -Path $outpath -ChildPath "sdelete.exe")  -UseBasicParsing
 
+Expand-Archive -LiteralPath (Join-Path -Path $outpath -ChildPath "sdelete.exe") -DestinationPath $outpath 
 
-# Clean-up Online image
-Dism.exe /online /Cleanup-Image /StartComponentCleanup
+Start-Process -NoNewWindow -FilePath (Join-Path -Path $outpath -ChildPath "sdelete.exe") -ArgumentList "-nobanner -z -c -p 1"
 
-# Clean-up and remove all superseded versions of every component in the component store
-# Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
+foreach($tpath in $tempPaths){
+    Write-Log -Level "INFO" -Message "Cleaning $($tpath)"
+
+    Write-Log -Level "INFO" -Message "Getting files in $($tpath)"
+    $tempFiles = Get-ChildItem $tpath -Recurse -File
+    # $tempFiles
+    Write-Log -Level "INFO" -Message "Removing files in $($tpath)"
+    $tempFiles | Remove-Item -Force #-ErrorAction SilentlyContinue
+
+    Write-Log -Level "INFO" -Message "Getting Directories in $($tpath)"
+    $tempDirs = Get-ChildItem $tpath -Recurse -Directory
+    # $tempFiles
+    Write-Log -Level "INFO" -Message "Removing Directories in $($tpath)"
+    $tempDirs | Remove-Item -Force -Recurse #-ErrorAction SilentlyContinue
+}
+
