6143bdbf5b5668cb6e6da794c5c4b3f7b624fe81
Matthew M <514050+mjmcfalls@users.noreply.github.com>
2022-06-05 14:21:17 -0400

    Add app stage

--- build_packer_stages.sh
+++ build_packer_stages.sh
@@ -5,6 +5,7 @@ log_name=packer_build_$current_date.log
 vm_name=Windows10_$current_date
 bare_output_path=/home/libvirt/images/pool/Win10/Win10_bare_$current_date
 base_output_path=/home/libvirt/images/pool/Win10/Win10_base_$current_date
+baseapp_output_path=/home/libvirt/images/pool/Win10/Win10_baseapp_$current_date
 packer_path=/home/mmcfalls/dev/packer
 
 export PACKER_LOG="1"
@@ -18,7 +19,7 @@ cd $packer_path
 # packer build -timestamp-ui -only 'qemu.Windows_10' -var-file vars/Windows10/Windows10.pkrvars.hcl -var-file secrets/secrets.pkrvars.hcl Windows10_parallel.pkr.hcl
 # Build OS with no changes
 echo "Starting win_iso.qemu.Windows10_iso - $bare_output_path"
-packer build -timestamp-ui -only 'win_iso.qemu.Windows10_iso' -var "keep_registered=true" -var "nix_output_directory=$bare_output_path" -var "vm_name=$vm_name" -var-file vars/Windows10/Windows10.pkrvars.hcl -var-file secrets/secrets.pkrvars.hcl Windows10_stages.pkr.hcl
+packer build -timestamp-ui -only 'win_iso.qemu.Windows10_iso' -var "keep_registered=false" -var "nix_output_directory=$bare_output_path" -var "vm_name=$vm_name" -var-file vars/Windows10/Windows10.pkrvars.hcl -var-file secrets/secrets.pkrvars.hcl Windows10_stages.pkr.hcl
 
 # Check if VM register if so, poweron, else register and poweron
 # set -x
@@ -34,11 +35,19 @@ fi
 # set +x
 
 # Get SHA256 hash of VM
-base_sha=$(sha256sum "$bare_output_path/$vm_name" | cut -d " " -f 1)
+bare_sha=$(sha256sum "$bare_output_path/$vm_name" | cut -d " " -f 1)
 # Build Base OS
 echo "Starting win_base.qemu.Windows10_base - $base_output_path"
 echo "iso_checksum=sha256:$base_sha"
 echo "iso_url=$bare_output_path/$vm_name"
 
-packer build -timestamp-ui -only 'win_base.qemu.Windows10_base' -var "keep_registered=true" -var "iso_checksum=sha256:$base_sha" -var iso_url=$bare_output_path/$vm_name -var "nix_output_directory=$base_output_path" -var "vm_name=$vm_name" -var-file vars/Windows10/Windows10.pkrvars.hcl -var-file secrets/secrets.pkrvars.hcl Windows10_stages.pkr.hcl
+packer build -timestamp-ui -only 'win_base.qemu.Windows10_base' -var "keep_registered=false" -var "iso_checksum=sha256:$bare_sha" -var iso_url=$base_output_path/$vm_name -var "nix_output_directory=$base_output_path" -var "vm_name=$vm_name" -var-file vars/Windows10/Windows10.pkrvars.hcl -var-file secrets/secrets.pkrvars.hcl Windows10_stages.pkr.hcl
 
+
+base_sha=$(sha256sum "$base_output_path/$vm_name" | cut -d " " -f 1)
+# Build Base + Apps1
+echo "Starting win_base_apps1.qemu.Windows10_base - $baseapp_output_path"
+echo "iso_checksum=sha256:$base_sha"
+echo "iso_url=$base_output_path/$vm_name"
+
+packer build -timestamp-ui -only 'win_base.qemu.Windows10_base' -var "keep_registered=false" -var "iso_checksum=sha256:$base_sha" -var iso_url=$base_output_path/$vm_name -var "nix_output_directory=$baseapp_output_path" -var "vm_name=$vm_name" -var-file vars/Windows10/Windows10.pkrvars.hcl -var-file secrets/secrets.pkrvars.hcl Windows10_stages.pkr.hcl
