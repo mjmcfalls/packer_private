d0e2d080446796cb99f3534a0b3eedf8a6ed3752
Matthew M <514050+mjmcfalls@users.noreply.github.com>
2022-07-07 16:36:22 -0400

    Enable homelab optimized and baseapps

--- build_packer_stages_homelab.sh
+++ build_packer_stages_homelab.sh
@@ -47,26 +47,26 @@ echo "iso_url=$bare_output_path/$vm_name"
 
 packer build -timestamp-ui -only 'win_base.qemu.Windows_base' -var "keep_registered=false" -var "iso_checksum=sha256:$bare_sha" -var iso_url=$bare_output_path/$vm_name -var "nix_output_directory=$base_output_path" -var "vm_name=$vm_name" -var-file vars/Windows_App_Vars.pkrvars.hcl -var-file vars/Windows10/Windows10.pkrvars.hcl -var-file secrets/secrets.pkrvars.hcl Windows10_stages_homelab.pkr.hcl
 
-# base_sha=$(sha256sum "$base_output_path/$vm_name" | cut -d " " -f 1)
-# # Build Base + Apps1
-# echo "Starting win_base_apps1.qemu.Windows_base - $baseapp_output_path"
-# echo "iso_checksum=sha256:$base_sha"
-# echo "iso_url=$base_output_path/$vm_name"
+base_sha=$(sha256sum "$base_output_path/$vm_name" | cut -d " " -f 1)
+# Build Base + Apps1
+echo "Starting win_base_apps1.qemu.Windows_base - $baseapp_output_path"
+echo "iso_checksum=sha256:$base_sha"
+echo "iso_url=$base_output_path/$vm_name"
 
-# export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/Win10_baseapp_$current_date"
-# packer build -timestamp-ui -only 'win_base_apps1.qemu.Windows_base' -var "keep_registered=false" -var "iso_checksum=sha256:$base_sha" -var iso_url=$base_output_path/$vm_name -var "nix_output_directory=$baseapp_output_path" -var "vm_name=$vm_name" -var-file vars/Windows_App_Vars.pkrvars.hcl -var-file vars/Windows10/Windows10.pkrvars.hcl -var-file secrets/secrets.pkrvars.hcl Windows10_stages_homelab.pkr.hcl
+export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/Win10_baseapp_$current_date"
+packer build -timestamp-ui -only 'win_base_apps1.qemu.Windows_base' -var "keep_registered=false" -var "iso_checksum=sha256:$base_sha" -var iso_url=$base_output_path/$vm_name -var "nix_output_directory=$baseapp_output_path" -var "vm_name=$vm_name" -var-file vars/Windows_App_Vars.pkrvars.hcl -var-file vars/Windows10/Windows10.pkrvars.hcl -var-file secrets/secrets.pkrvars.hcl Windows10_stages_homelab.pkr.hcl
 
-# baseapp_sha=$(sha256sum "$baseapp_output_path/$vm_name" | cut -d " " -f 1)
+baseapp_sha=$(sha256sum "$baseapp_output_path/$vm_name" | cut -d " " -f 1)
 
-# # Optimize VMs
-# export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/Win10_base_opt_$current_date"
-# packer build -timestamp-ui -only 'win_base_optimize.qemu.Windows_base' -var "keep_registered=false" -var "iso_checksum=sha256:$base_sha" -var iso_url=$base_output_path/$vm_name -var "nix_output_directory=$base_opt_output_path" -var "vm_name=$vm_name" -var-file vars/Windows_App_Vars.pkrvars.hcl -var-file vars/Windows10/Windows10.pkrvars.hcl -var-file secrets/secrets.pkrvars.hcl Windows10_stages_homelab.pkr.hcl
-# export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/Win10_baseapp_opt_$current_date"
-# packer build -timestamp-ui -only 'win_base_optimize.qemu.Windows_base' -var "keep_registered=false" -var "iso_checksum=sha256:$baseapp_sha" -var iso_url=$baseapp_output_path/$vm_name -var "nix_output_directory=$baseapp_opt_output_path" -var "vm_name=$vm_name" -var-file vars/Windows_App_Vars.pkrvars.hcl -var-file vars/Windows10/Windows10.pkrvars.hcl -var-file secrets/secrets.pkrvars.hcl Windows10_stages_homelab.pkr.hcl
+# Optimize VMs
+export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/Win10_base_opt_$current_date"
+packer build -timestamp-ui -only 'win_base_optimize.qemu.Windows_base' -var "keep_registered=false" -var "iso_checksum=sha256:$base_sha" -var iso_url=$base_output_path/$vm_name -var "nix_output_directory=$base_opt_output_path" -var "vm_name=$vm_name" -var-file vars/Windows_App_Vars.pkrvars.hcl -var-file vars/Windows10/Windows10.pkrvars.hcl -var-file secrets/secrets.pkrvars.hcl Windows10_stages_homelab.pkr.hcl
+export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/Win10_baseapp_opt_$current_date"
+packer build -timestamp-ui -only 'win_base_optimize.qemu.Windows_base' -var "keep_registered=false" -var "iso_checksum=sha256:$baseapp_sha" -var iso_url=$baseapp_output_path/$vm_name -var "nix_output_directory=$baseapp_opt_output_path" -var "vm_name=$vm_name" -var-file vars/Windows_App_Vars.pkrvars.hcl -var-file vars/Windows10/Windows10.pkrvars.hcl -var-file secrets/secrets.pkrvars.hcl Windows10_stages_homelab.pkr.hcl
 
 
-echo "virt-install --name "Win10_base_$current_date" --memory 8192 --vcpus 6 --disk bus=virtio,path=$base_output_path/$vm_name --network bridge:br0 --import --os-variant win10"
-virt-install --name "Win10_base_$current_date" --memory 8192 --vcpus 6 --disk bus=virtio,path=$base_output_path/$vm_name --network bridge:br0 --import --os-variant win10
+# echo "virt-install --name "Win10_base_$current_date" --memory 8192 --vcpus 6 --disk bus=virtio,path=$base_output_path/$vm_name --network bridge:br0 --import --os-variant win10"
+# virt-install --name "Win10_base_$current_date" --memory 8192 --vcpus 6 --disk bus=virtio,path=$base_output_path/$vm_name --network bridge:br0 --import --os-variant win10
 # virt-install --name "Win10_baseapp_$current_date" --memory 8192 --vcpus 6 --disk "$baseapp_output_path/$vm_name" --import --os-variant win10
-
-
+virt-install --name "Win10_base_opt_$current_date" --memory 8192 --vcpus 6 --disk bus=virtio,path=$base_opt_output_path/$vm_name --network bridge:br0 --import --os-variant win10
+virt-install --name "Win10_baseapp_opt_$current_date" --memory 8192 --vcpus 6 --disk bus=virtio,path=$baseapp_opt_output_path/$vm_name --network bridge:br0 --import --os-variant win10
