e7de8ff4ccb465cd62c7b6a49ea47fbfa82be2aa
Matthew M <514050+mjmcfalls@users.noreply.github.com>
2022-09-14 14:01:45 -0400

    Update iso echo variable

--- build_packer_stages_homelab.sh
+++ build_packer_stages_homelab.sh
@@ -2,15 +2,18 @@
 
 current_date=$(date "+%Y%m%d%H%M%S")
 log_name=packer_build_$current_date.log
-vm_name=Windows10_$current_date
-bare_output_path=/home/libvirt/images/pool/Win10/Win10_bare_$current_date
-base_output_path=/home/libvirt/images/pool/Win10/Win10_base_$current_date
-base_opt_output_path=/home/libvirt/images/pool/Win10/Win10_base_opt_$current_date
-baseapp_output_path=/home/libvirt/images/pool/Win10/Win10_baseapp_$current_date
-baseapp_opt_output_path=/home/libvirt/images/pool/Win10/Win10_baseapp_opt_$current_date
 
-baseapptwo_output_path=/home/libvirt/images/pool/Win10/Win10_baseapptwo_$current_date
-baseapptwo_opt_output_path=/home/libvirt/images/pool/Win10/Win10_baseapptwo_opt_$current_date
+os_name=Server2022
+vm_name=$os_name-$current_date
+
+bare_output_path=/home/libvirt/images/pool/$os_name/$os_name-bare-$current_date
+base_output_path=/home/libvirt/images/pool/$os_name/$os_name-base-$current_date
+base_opt_output_path=/home/libvirt/images/pool/$os_name/$os_name-base-opt-$current_date
+baseapp_output_path=/home/libvirt/images/pool/$os_name/$os_name-baseapp-$current_date
+baseapp_opt_output_path=/home/libvirt/images/pool/$os_name/$os_name-baseapp-opt-$current_date
+
+baseapptwo_output_path=/home/libvirt/images/pool/$os_name/$os_name-baseapptwo-$current_date
+baseapptwo_opt_output_path=/home/libvirt/images/pool/$os_name/$os_name-baseapptwo-opt-$current_date
 
 packer_path=/home/mmcfalls/dev/packer
 
@@ -31,7 +34,7 @@ cd $packer_path
 
 # packer build -timestamp-ui -only 'qemu.Windows_10' -var-file vars/Windows10/Windows10.pkrvars.hcl -var-file secrets/secrets.pkrvars.hcl Windows10_parallel.pkr.hcl
 # Build OS with no changes
-export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/Win10_bare_$current_date"
+export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/$os_name-bare-$current_date"
 echo "Starting win_iso.qemu.win_iso - $bare_output_path"
 packer build -timestamp-ui -only 'win_iso.qemu.win_iso' -var "keep_registered=false" -var "nix_output_directory=$bare_output_path" -var "vm_name=$vm_name" -var-file vars/Windows_App_Vars.pkrvars.hcl -var-file $os_vars -var-file $secret_vars $build_file
 
@@ -51,10 +54,11 @@ fi
 # Get SHA256 hash of VM
 echo "Generating SHA256 checksum for $bare_output_path/$vm_name"
 bare_sha=$(sha256sum "$bare_output_path/$vm_name" | cut -d " " -f 1)
+
 # Build Base OS
-export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/Win10_base_$current_date"
+export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/$os_name-base-$current_date"
 echo "Starting win_base.qemu.Windows_base - $base_output_path"
-echo "iso_checksum=sha256:$base_sha"
+echo "iso_checksum=sha256:$bare_sha"
 echo "iso_url=$bare_output_path/$vm_name"
 
 packer build -timestamp-ui -only 'win_base.qemu.Windows_base' -var "keep_registered=false" -var "iso_checksum=sha256:$bare_sha" -var iso_url=$bare_output_path/$vm_name -var "nix_output_directory=$base_output_path" -var "vm_name=$vm_name" -var-file vars/Windows_App_Vars.pkrvars.hcl -var-file $os_vars -var-file $secret_var $build_file
@@ -67,7 +71,7 @@ echo "Starting win_base_apps1.qemu.Windows_base - $baseapp_output_path"
 echo "iso_checksum=sha256:$base_sha"
 echo "iso_url=$base_output_path/$vm_name"
 
-export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/Win10_baseapp_$current_date"
+export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/$os_name-baseapp-$current_date"
 packer build -timestamp-ui -only 'win_base_apps1.qemu.Windows_base' -var "keep_registered=false" -var "iso_checksum=sha256:$base_sha" -var iso_url=$base_output_path/$vm_name -var "nix_output_directory=$baseapp_output_path" -var "vm_name=$vm_name" -var-file vars/Windows_App_Vars.pkrvars.hcl -var-file $os_vars -var-file $secret_var $build_file
 
 echo "Generating SHA256 checksum for $baseapp_output_path/$vm_name"
@@ -80,7 +84,7 @@ echo "Starting win_base_apps2.qemu.Windows_base - $baseapptwo_output_path"
 echo "iso_checksum=sha256:$base_sha"
 echo "iso_url=$base_output_path/$vm_name"
 
-export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/Win10_baseapptwo_$current_date"
+export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/$os_name-baseapptwo-$current_date"
 packer build -timestamp-ui -only 'win_base_apps2.qemu.Windows_base' -var "keep_registered=false" -var "iso_checksum=sha256:$base_sha" -var iso_url=$base_output_path/$vm_name -var "nix_output_directory=$baseapptwo_output_path" -var "vm_name=$vm_name" -var-file vars/Windows_App_Vars.pkrvars.hcl -var-file $os_vars -var-file $secret_var $build_file
 
 echo "Generating SHA256 checksum for $baseapptwo_output_path/$vm_name"
@@ -90,48 +94,48 @@ baseapptwo_sha=$(sha256sum "$baseapptwo_output_path/$vm_name" | cut -d " " -f 1)
 echo "win_base_optimize.qemu.Windows_base - $base_opt_output_path"
 echo "iso_checksum=sha256:$base_sha"
 echo "iso_url=$base_output_path/$vm_name"
-export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/Win10_base_opt_$current_date"
+export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/$os_name-base_opt-$current_date"
 packer build -timestamp-ui -only 'win_base_optimize.qemu.Windows_base' -var "keep_registered=false" -var "iso_checksum=sha256:$base_sha" -var iso_url=$base_output_path/$vm_name -var "nix_output_directory=$base_opt_output_path" -var "vm_name=$vm_name" -var-file vars/Windows_App_Vars.pkrvars.hcl -var-file $os_vars -var-file $secret_var $build_file
 
 echo "win_base_optimize.qemu.Windows_base - $baseapp_output_path"
 echo "iso_checksum=sha256:$baseapp_sha"
 echo "iso_url=$baseapp_output_path/$vm_name"
-export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/Win10_baseapp_opt_$current_date"
+export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/$os_name-baseapp-opt-$current_date"
 packer build -timestamp-ui -only 'win_base_optimize.qemu.Windows_base' -var "keep_registered=false" -var "iso_checksum=sha256:$baseapp_sha" -var iso_url=$baseapp_output_path/$vm_name -var "nix_output_directory=$baseapp_opt_output_path" -var "vm_name=$vm_name" -var-file vars/Windows_App_Vars.pkrvars.hcl -var-file $os_vars -var-file $secret_var $build_file
 
 echo "win_base_optimize.qemu.Windows_base - $baseapptwo_output_path"
 echo "iso_checksum=sha256:$baseapptwo_sha"
 echo "iso_url=$baseapptwo_output_path/$vm_name"
-export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/Win10_baseapptwo_opt_$current_date"
+export PACKER_LOG_PATH="/home/mmcfalls/dev/logs/$os_name-baseapptwo-opt-$current_date"
 packer build -timestamp-ui -only 'win_base_optimize.qemu.Windows_base' -var "keep_registered=false" -var "iso_checksum=sha256:$baseapptwo_sha" -var iso_url=$baseapptwo_output_path/$vm_name -var "nix_output_directory=$baseapptwo_opt_output_path" -var "vm_name=$vm_name" -var-file vars/Windows_App_Vars.pkrvars.hcl -var-file $os_vars -var-file $secret_var $build_file
 
 
 echo "Starting Virt-install"
-# echo "virt-install --name "Win10_base_$current_date" --memory 8192 --vcpus 6 --disk bus=virtio,path=$base_output_path/$vm_name --network bridge:br0 --import --os-variant win10"
-# virt-install --name "Win10_base_$current_date" --memory 8192 --vcpus 6 --disk bus=virtio,path=$base_output_path/$vm_name --network bridge:br0 --import --os-variant win10
-# virt-install --name "Win10_baseapp_$current_date" --memory 8192 --vcpus 6 --disk "$baseapp_output_path/$vm_name" --import --os-variant win10
+# echo "virt-install --name "$os_name_base_$current_date" --memory 8192 --vcpus 6 --disk bus=virtio,path=$base_output_path/$vm_name --network bridge:br0 --import --os-variant win10"
+# virt-install --name "$os_name_base_$current_date" --memory 8192 --vcpus 6 --disk bus=virtio,path=$base_output_path/$vm_name --network bridge:br0 --import --os-variant win10
+# virt-install --name "$os_name_baseapp_$current_date" --memory 8192 --vcpus 6 --disk "$baseapp_output_path/$vm_name" --import --os-variant win10
 
-echo "virt-install --name Win10_base_opt_$current_date --memory 8192 --vcpus 6 --disk bus=virtio,path=$base_opt_output_path/$vm_name --network bridge:br0 --import --os-variant win10 --noautoconsole"
-virt-install --name Win10_base_opt_$current_date --memory 8192 --vcpus 6 --disk bus=virtio,path=$base_opt_output_path/$vm_name --network bridge:br0 --import --os-variant win10 --noautoconsole
+echo "virt-install --name $os_name-base-opt-$current_date --memory 8192 --vcpus 6 --disk bus=virtio,path=$base_opt_output_path/$vm_name --network bridge:br0 --import --os-variant win10 --noautoconsole"
+virt-install --name $os_name-base-opt-$current_date --memory 8192 --vcpus 6 --disk bus=virtio,path=$base_opt_output_path/$vm_name --network bridge:br0 --import --os-variant win10 --noautoconsole
 
-echo "virt-install --name Win10_baseapp_opt_$current_date --memory 8192 --vcpus 6 --disk bus=virtio,path=$baseapp_opt_output_path/$vm_name --network bridge:br0 --import --os-variant win10 --noautoconsole"
-virt-install --name Win10_baseapp_opt_$current_date --memory 8192 --vcpus 6 --disk bus=virtio,path=$baseapp_opt_output_path/$vm_name --network bridge:br0 --import --os-variant win10 --noautoconsole
+echo "virt-install --name $os_name-baseapp-opt-$current_date --memory 8192 --vcpus 6 --disk bus=virtio,path=$baseapp_opt_output_path/$vm_name --network bridge:br0 --import --os-variant win10 --noautoconsole"
+virt-install --name $os_name-baseapp-opt-$current_date --memory 8192 --vcpus 6 --disk bus=virtio,path=$baseapp_opt_output_path/$vm_name --network bridge:br0 --import --os-variant win10 --noautoconsole
 
-echo "virt-install --name Win10_baseapptwo_opt_$current_date --memory 8192 --vcpus 6 --disk bus=virtio,path=$baseapptwo_opt_output_path/$vm_name --network bridge:br0 --import --os-variant win10 --noautoconsole"
-virt-install --name Win10_baseapptwo_opt_$current_date --memory 8192 --vcpus 6 --disk bus=virtio,path=$baseapptwo_opt_output_path/$vm_name --network bridge:br0 --import --os-variant win10 --noautoconsole
+echo "virt-install --name $os_name-baseapptwo-opt-$current_date --memory 8192 --vcpus 6 --disk bus=virtio,path=$baseapptwo_opt_output_path/$vm_name --network bridge:br0 --import --os-variant win10 --noautoconsole"
+virt-install --name $os_name-baseapptwo-opt-$current_date --memory 8192 --vcpus 6 --disk bus=virtio,path=$baseapptwo_opt_output_path/$vm_name --network bridge:br0 --import --os-variant win10 --noautoconsole
 
 
-echo "Adding SCSI Controller to Win10_base_opt_$current_date"
-virsh attach-device --config Win10_base_opt_$current_date /home/mmcfalls/dev/packer/src/scripts/kvm/scsi-controller.xml
+echo "Adding SCSI Controller to $os_name-base-opt-$current_date"
+virsh attach-device --config $os_name-base-opt-$current_date /home/mmcfalls/dev/packer/src/scripts/kvm/scsi-controller.xml
 
-echo "Adding SCSI Controller to Win10_baseapp_opt_$current_date" 
-virsh attach-device --config Win10_baseapp_opt_$current_date /home/mmcfalls/dev/packer/src/scripts/kvm/scsi-controller.xml
+echo "Adding SCSI Controller to $os_name-baseapp-opt-$current_date" 
+virsh attach-device --config $os_name-baseapp-opt-$current_date /home/mmcfalls/dev/packer/src/scripts/kvm/scsi-controller.xml
 
-echo "Starting Win10_baseapp_opt_$current_date" 
-virsh start Win10_baseapp_opt_$current_date
+echo "Starting $os_name-baseapp-opt-$current_date" 
+virsh start $os_name-baseapp-opt-$current_date
 
-echo "Starting Win10_base_opt_$current_date"
-virsh start Win10_base_opt_$current_date
+echo "Starting $os_name-base-opt-$current_date"
+virsh start $os_name-base-opt-$current_date
 
-echo "Adding Bluray Drive to Win10_baseapp_opt_$current_date" 
-virsh attach-device --config Win10_baseapp_opt_$current_date /home/mmcfalls/dev/packer/src/scripts/kvm/bluray-drive.xml
+echo "Adding Bluray Drive to $os_name-baseapp-opt-$current_date" 
+virsh attach-device --config $os_name-baseapp-opt-$current_date /home/mmcfalls/dev/packer/src/scripts/kvm/bluray-drive.xml
