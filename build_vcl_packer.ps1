14dd5f3c5152d62a7ddccb40969af9d262642c11
Matthew M <514050+mjmcfalls@users.noreply.github.com>
2022-10-07 09:50:30 -0400

    refactor build script

--- build_vcl_packer.ps1
+++ build_vcl_packer.ps1
@@ -4,18 +4,21 @@ Param (
     [int]$debugging = 1,
     $postfix = (Get-Date -format yyyyMMddhhmm),
     [string]$outPath = "c:\temp\",
-    [string]$debugLog = "$($outpath)\packerlog_$($postfix).log",
-    [string]$varsfile = ".\vars\Windows_VCL\Windows_vcl.pkrvars.hcl",
-    [string]$appvarFile = "vars\Windows_VCL\Windows_VCL_App_Vars.pkrvars.hcl",
-    [string]$secretsfile = ".\secrets\secrets.pkrvars.hcl",
-    [string]$buildfile = ".\Windows10_vcl.pkr.hcl",
-    [string]$packerpath = ".\bin\packer.exe",
-    [string]$vm_name = "Windows10",
-    [string]$keepregistered = "false",
-    [string]$isoPath,
-    [string]$isoSha,
-    [string]$unattendPath = "C:\Users\mmcfall\Desktop\dev\UnattendFiles\Windows10_ltsb\autounattend.xml",
-    [string]$buildTarget = "windows10.vmware-iso.win_iso"
+    [string]$configFile = ".\packer_vcl_build_vars.json",
+    $targetOS,
+    [switch]$vmtimestamp
+    # [string]$debugLog = "$($outpath)\packerlog_$($postfix).log",
+    # [string]$varsfile = ".\vars\Windows_VCL\Windows_vcl.pkrvars.hcl",
+    # [string]$appvarFile = "vars\Windows_VCL\Windows_VCL_App_Vars.pkrvars.hcl",
+    # [string]$secretsfile = ".\secrets\secrets.pkrvars.hcl",
+    # [string]$buildfile = ".\vcl.pkr.hcl",
+    # [string]$packerpath = ".\bin\packer.exe",
+    # [string]$vm_name = "Windows10",
+    # [string]$keepregistered = "false",
+    # [string]$isoPath,
+    # [string]$isoSha,
+    # [string]$unattendPath = "C:\Users\mmcfall\Desktop\dev\UnattendFiles\Windows10_ltsb\autounattend.xml",
+    # [string]$buildTarget = "windows10.vmware-iso.win_iso"
 )
 
 Function Write-Log {
@@ -39,35 +42,97 @@ Function Write-Log {
         Add-Content $logfile -Value $Line
     }
     Else {
-        Write-Output $Line
+        Write-Host $Line
     }
+}
+
+Function Start-PackerBuild {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $False)][string]$logfile,
+        $build,
+        $vmtimestamp
+    )
+
+    $vm_build_stopWatch = [System.Diagnostics.StopWatch]::StartNew()
+
+    Write-Log -Level "INFO" -Message "$($build.vm_name) - Start of Packer Build" -logfile $logfile
+
+    if($vmtimestamp.IsPresent){
+        Write-Log -Level "INFO" -Message "$($build.vm_name) - Appending Timestamp to VM Name; Current VM Name: $($build.vm_name)" -logfile $logfile
+        $build.vm_name = "$($build.vm_name)_$(Get-Date -Format yyyyMMddhhmm)"
+        Write-Log -Level "INFO" -Message "$($build.vm_name) - New VM Name: $($build.vm_name)" -logfile $logfile
+    }
+
+
+    Write-Log -Level "INFO" -Message "$($build.vm_name) - Build Parameters:" -logfile $logfile
+    Foreach($item in $build.PSObject.Properties){
+        Write-Log -Level "INFO" -Message "$($build.vm_name) - $($item.Name): $($item.value)"
+        # $item.Name
+        # $item.value
+    }
+
+    if ($build.debug -like "1") {
+        $env:PACKER_LOG = $debug
+        $env:PACKER_LOG_PATH = Join-path -Path $outPath -ChildPath "$($build.vm_name).log"
+        # $build
+        Write-Log -Level "INFO" -Message "$($build.vm_name) - Debugging log enabled" -logfile $logfile
+        Write-Log -Level "INFO" -Message "$($build.vm_name) - Logging to $($env:PACKER_LOG_PATH)" -logfile $logfile
+    }
+    else {
+        Write-Log -Level "INFO" -Message "$($build.vm_name) - Debugging log disabled" -logfile $logfile
+    }
+
+
+    Write-Log -Level "INFO" -Message "$($build.vm_name) - Building from $($build.isoPath)" -logfile $logfile
+
+
+    Write-Log -Level "INFO" -Message "$($build.packerpath) build -timestamp-ui -only $($build.buildTarget) -var `"autounattend=$($build.unattendPath)`" -var `"iso_checksum=$($build.isoSha)`" -var `"iso_url=$($build.isoPath)`" -var `"vm_name=$($build.vm_name)`" -var-file $($build.varsfile) -var-file $($build.appvarFile)  -var-file $($build.secretsfile) $($build.buildfile)"  -logfile $logfile
+
+    # Start-Process -NoNewWindow -FilePath "$($packerpath)" -ArgumentList "build -timestamp-ui -only $build.buildTarget -var `"autounattend=$($build.unattendPath)`" -var `"iso_checksum=$($build.isoSha)`" -var `"iso_url=$($build.isoPath)`" -var `"vm_name=$($build.vm_name)`" -var-file $($build.varsfile) -var-file $($build.appvarFile) -var-file $($build.secretsfile) $($build.buildfile)" -Wait
+
+    Write-Log -Level "INFO" -Message "$($build.vm_name) - End Packer build" -logfile $logfile
+
+    $vm_build_stopWatch.Stop()
+    $build.stopwatch = $vm_build_stopWatch
+    
+    Write-Log -Level "INFO" -Message "$($build.vm_name) - Build Time: $($vm_build_stopWatch.Elapsed)" -logfile $logfile
+
+    $build
 }
 
 # Start Script Stop watch
 $scriptStopWatch = [System.Diagnostics.StopWatch]::StartNew()
 
+if (Test-Path $configFile) {
+    Try {
+        $buildVariables = Get-Content -Raw -Path $configFile | ConvertFrom-Json
+    }
+    catch {
+        Write-Log -Level "INFO" -Message "Error reaching $($configFile)"
+        Exit 1
+    }
+}
+else {
+    Write-Log -Level "INFO" -Message "Exitings - $($configFile) not found"
+    Exit 1
+}
 
-# Virtual machine names
-$vm_name_postfix = "$($vm_name)_$($postfix)"
 
-# Set ENV Variables for debugging
-$env:PACKER_LOG = $debugging
-$env:PACKER_LOG_PATH = $debugLog
-
-$vm_name_postfix_StopWatch = [System.Diagnostics.StopWatch]::StartNew()
-Write-Log -Level "INFO" -Message "Building $($vm_name_postfix) from $($isoPath)"
-
-$env:PACKER_LOG_PATH = "$($outPath)\$($vm_name_postfix).log"
-
-Write-Log -Level "DEBUG" -Message "$($packerpath) build -timestamp-ui -only $buildTarget -var `"autounattend=$($unattendPath)`" -var `"iso_checksum=$($isoSha)`" -var `"iso_url=$($isoPath)`" -var `"vm_name=$($vm_name_postfix)`" -var-file $($varsfile) -var-file $($appvarFile)  -var-file $($secretsfile) $($buildfile)" 
-Start-Process -NoNewWindow -FilePath "$($packerpath)" -ArgumentList "build -timestamp-ui -only $buildTarget -var `"autounattend=$($unattendPath)`" -var `"iso_checksum=$($isoSha)`" -var `"iso_url=$($isoPath)`" -var `"vm_name=$($vm_name_postfix)`" -var-file $($varsfile) -var-file $($appvarFile) -var-file $($secretsfile) $($buildfile)" -Wait
-
-Write-Log -Level "INFO" -Message "End Build $($vm_name_postfix) from $($isoPath)"
-
-$vm_name_postfix_StopWatch.Stop()
+Foreach ($os in $targetOS) {
+    # $buildVariables.Windows10
+    Write-Log -Level "INFO" -Message "$($os) - ===== Start build =====" -logfile $logfile
+    if ($buildVariables.$os) {
+        Write-Log -Level "INFO" -Message "$($os) - Found in build variables" -logfile $logfile
+        $buildVariables.$os = Start-PackerBuild -build $buildVariables.$os -vmtimestamp $vmtimestamp
+        $buildVariables.$os.stopwatch
+    }
+    else {
+        Write-Log -Level "INFO" -Message "$($os) - Not Found in build variables" -logfile $logfile
+    }
+    Write-Log -Level "INFO" -Message "$($os) - ===== End Build =====" -logfile $logfile
+}
 
 $scriptStopWatch.Stop()
 
-
-Write-Log -Level "INFO" -Message "Bare Build Time: $($vm_name_postfix_StopWatch.Elapsed)"
-Write-Log -Level "INFO" -Message "Build Script Time: $($scriptStopWatch.Elapsed)"
+Write-Log -Level "INFO" -Message "Build Script Time: $($scriptStopWatch.Elapsed)" -logfile $logfile
